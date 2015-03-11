1f93ab2eb6605b445f30d41cb97e52781ae368f0
K.S.Krishna Chandran <ks.krishnan1996@gmail.com>
2015-03-11 22:22:04 +0530

    Program to add two numbers

--- add.c
+++ add.c
@@ -1,23 +1,23 @@
 
 
 
-
-
 #include<stdio.h>
 
-int main(int argc,int argv[]) {
+int main(int argc, int *argv[]) {
 	system("clear");
 	int i;
-	int sum = 0;
+	int *sum = 0;
 	if(argc < 3) {
 		printf("\n\nPlease enter two numbers ");
 		return 0;
 	}
+	int *temp;
 	for(i = 0; i < argc-1; i++) {
-		sum = sum + argv[i];
+		(*(temp)) = (*(argv +i));
+		(*(sum)) = (*(sum)) + (*(temp));
 	}
-	
-	printf("%d + %d = %d", argv[1], argv[2], sum);
+	printf("%d", (*(sum)));
+	//printf("\n%d + %d = %d", (*(argv+1)), (*(argv+2)), sum);
 	printf("\n\n");
 	return 0;
 }
---
+++ add1.c
@@ -0,0 +1,13 @@
+
+
+
+
+#include<stdio.h>
+
+int main(int argc, int *argv[]) {
+	system("clear");
+	printf("%d %d",argv[1],argv[2]); 
+	printf("\n\n");
+	return 0;
+}
+
